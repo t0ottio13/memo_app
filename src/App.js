646541fc79afa32fb83143715f794b8305d68275
t0ottio13 <adsoption104@gmail.com>
2021-05-09 19:55:50 +0900

    Add Basic_features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import MemoPage from './memo/MemoPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        hogehoge
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary text-white display-4">Memo App by React</h1>
+      <div className="container mt-3">
+        <h4 className="my-3">Memo.</h4>
+        < MemoPage />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
